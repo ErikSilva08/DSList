8f9e3f3c1597406bc8127e47be5545f8560bc401
ErikSilva08 <eriksilvaf89@gmail.com>
2025-01-21 09:59:17 -0300

    game seed and optimizations

--- src/main/java/com/erikdev/DSList/entity/Game.java
+++ src/main/java/com/erikdev/DSList/entity/Game.java
@@ -14,19 +14,25 @@ public class Game {
     private Integer gameYear;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer gameYear, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer gameYear, String genre, String platforms,Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.gameYear = gameYear;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -70,6 +76,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
